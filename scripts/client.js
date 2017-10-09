e5522de46b6f3dbdb57e3a942ae8263184d22f69
Marta Jopp <martapeterson@gmail.com>
2017-10-09 14:36:00 -0500

    swap and delete button

--- scripts/client.js
+++ scripts/client.js
@@ -4,15 +4,27 @@ $(document).ready(readyNow);
 
 function readyNow(){
 $('#GenerateBtn').on('click', generate);
+$('#container').on('click', '#Swap', yellow);
+$('#container').on('click', '#Delete', remove);
 }
 
 var clickCount = 1;
 
 function generate(){
 console.log('Generate Clicked');
-  $('#container').append('<div id = "content"></div>');
-  $('#content').append('<p id="Number of Clicks">' + clickCount + '</p>');
+  $('#container').append('<div class = "content"> </div>');
+  $('.content').append('<p id="Number of Clicks">' + clickCount + '</p>');
 clickCount += 1;
-$('#content').append('<button id="Swap">Swap</button><button id="Delete">Delete</button>');
+$('.content').append('<button id="Swap">Swap</button><button id="Delete">Delete</button>');
 
 }//end generate
+
+function yellow(){
+  console.log('Swap Clicked');
+$(this).parent().css("background-color", "yellow");
+}
+
+function remove(){
+  console.log('Delete');
+  $(this).parent().remove();
+}
